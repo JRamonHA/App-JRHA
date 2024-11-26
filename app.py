cef3f4acfd47cb323328e4e5696b42968b427ab7
JRamonHA <hdezagjr@gmail.com>
2024-11-25 21:34:24 -0600

    Update

--- app.py
+++ app.py
@@ -8,3 +8,19 @@ tmx = pd.read_csv(f, index_col=0, parse_dates=True)
 columnas = list(tmx.columns)
 
 ui.page_opts(title="Web App")
+
+with ui.sidebar():
+        ui.input_select("var", "Selecciona una variable", choices=columnas)
+
+@render_widget
+def grafica():
+    var = input.var()
+    df = tmx.resample('ME').mean().reset_index()
+    fig = px.line(
+            df,
+            x='time',  
+            y=var,
+            title=f"{var}",
+            labels={'time': 'Fecha', var: var}
+        )
+    return fig
