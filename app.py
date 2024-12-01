3b3a6d5f3302250916dd9dec881cd9e432097a9f
JRamonHA <hdezagjr@gmail.com>
2024-12-01 12:26:36 -0600

    Add download

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ import pandas as pd
 import plotly.express as px
 from shiny import App, Inputs, reactive, render, ui
 from shinywidgets import render_widget, output_widget
+import faicons
+import io
 import funciones as func
 
 
 f = func.cargar_anio("2010")
 esol = pd.read_parquet(f)
 nombres = list(esol.columns)
-years = func.years  
+years = func.years
 
 
 app_ui = ui.page_navbar(
@@ -23,7 +25,16 @@ app_ui = ui.page_navbar(
     ),
     ui.nav_panel(
         "Datos",
-        ui.card(ui.output_data_frame("data")),
+        ui.card(
+            ui.card_header(
+                ui.span(ui.output_text("file_title"),
+                    class_="me-auto"),
+                ui.download_link("download_data", "Descargar archivo", icon=faicons.icon_svg("download"),
+                    class_="btn btn-primary btn-sm"),
+                class_="d-flex justify-content-between align-items-center"
+            ),
+            ui.output_data_frame("data"),
+        ),
     ),
     sidebar=ui.sidebar(
         ui.input_select("year", "Año", choices=list(years), selected="2010"),
@@ -75,7 +86,20 @@ def server(input: Inputs):
 
     @render.data_frame
     def data():
-        return cargar_esol()
+        df = cargar_esol().reset_index().rename(columns={"TIMESTAMP": "Fecha"})
+        return df
+
+    @render.text
+    def file_title():
+        return f"ESOLMET_{input.year()}.csv"
+
+    @render.download(filename=lambda: f"ESOLMET_{input.year()}.csv")
+    def download_data():
+        df = cargar_esol().reset_index()
+        df = df.rename(columns={"TIMESTAMP": "Fecha"}) 
+        with io.StringIO() as buf:
+            df.to_csv(buf, index=False)
+            yield buf.getvalue().encode()
 
 
 app = App(app_ui, server)
